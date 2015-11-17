c89e2f5cc0840d170f7461589befd20e170a9eb6
juan2190 <jcbs.219@gmail.com>
2015-11-16 23:48:28 -0500

    Initial Template

---
+++ testSales/StoreLocationViewController.swift
@@ -0,0 +1,44 @@
+//
+//  StoreLocationViewController.swift
+//  testSales
+//
+//  Created by MCS on 11/16/15.
+//  Copyright © 2015 balderas.juan. All rights reserved.
+//
+
+import UIKit
+
+class StoreLocationViewController: UIViewController {
+
+    
+    @IBOutlet var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
